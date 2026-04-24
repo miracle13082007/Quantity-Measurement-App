1d89df20b76225c9a274009437d636e30ecd6837
miracle13082007 <miracle13082007@gmail.com>
2026-04-24 09:29:49 +0530

    UC2: Feet and Inches measurement equality

--- src/UseCase.java
+++ src/UseCase.java
@@ -1,4 +1,4 @@
-public class QuantityMeasurementApp {
+public class UseCase {
 
     static class Feet {
         private final double value;
@@ -20,10 +20,40 @@ public class QuantityMeasurementApp {
         }
     }
 
-    public static void main(String[] args) {
-        Feet f1 = new Feet(1.0);
-        Feet f2 = new Feet(1.0);
+    static class Inches {
+        private final double value;
 
-        System.out.println(f1.equals(f2));
+        public Inches(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (obj == null || getClass() != obj.getClass()) {
+                return false;
+            }
+            Inches other = (Inches) obj;
+            return Double.compare(this.value, other.value) == 0;
+        }
+    }
+
+    public static boolean compareFeet(double v1, double v2) {
+        Feet f1 = new Feet(v1);
+        Feet f2 = new Feet(v2);
+        return f1.equals(f2);
+    }
+
+    public static boolean compareInches(double v1, double v2) {
+        Inches i1 = new Inches(v1);
+        Inches i2 = new Inches(v2);
+        return i1.equals(i2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(compareInches(1.0, 1.0));
+        System.out.println(compareFeet(1.0, 1.0));
     }
 }
